c46e8f0727b5c0b122b81cbeb8ad4243fa889109
LewisErick <repos.zul@gmail.com>
2016-03-29 14:12:02 -0600

    Minor changes

--- www/js/controllers/register.js
+++ www/js/controllers/register.js
@@ -6,9 +6,23 @@ app.controller('Register', function($scope, $interval, $ionicModal, $location, $
     $scope.age;
     $scope.marital;
     $scope.municipality;
+    $scope.muns;
+    $scope.house_phone;
+    $scope.cel_phone;
 
     $scope.register = function() {
         console.log("Validate registration.");
         console.log("Register!");
+        var form = {
+            name:$scope.name, 
+            last:$scope.last, 
+            age:$scope.age, 
+            marital:$scope.marital,
+            municipality:$scope.municipality, 
+            email:email
+        };
+        { "_id" : 2, "username" : "Erick", "name" : "Erick", "password" : "erick", "email" : "lewis.erick@outlook.com", "profile_picture" : "0.jpg", "friends" : [ 1 ], "pets" : [ 1 ] }
+
+        api.registerUser()
     };
 });
